5f4cc1b2fa3bd8ffe79b45c4e75cbc944de9e4ed
bernhardblietz <bernhard.blietz@mailbox.org>
2026-07-01 11:33:08 +0200

    fix: correct web dependency that keeps backend alive

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-h2console")
 	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework:spring-web")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
